4165c1b8979b21084f193ac3d035f8edc1944bdc
kyrian <dr.kyrian.onwe@gmail.com>
2021-11-19 18:24:13 -0600

    commit tree 985dcd1367282e186cea3be1aacf504ae58067f9

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+const { array } = require("yargs");
+
 // Code your solution here
+function findMatching(array, name){
+  const driveChecker = array.filter(function(driver){
+    if (name.toUpperCase() === driver.toUpperCase()){
+      return driver;
+    }
+  })
+  return driveChecker;
+}
+
+function fuzzyMatch(array, name){
+  const driverCheck = array.filter(function(driver){
+    if (name[0].toUpperCase() === driver[0].toUpperCase()){
+      return driver;
+    }
+  })
+  return driverCheck;
+}
+
+function matchName(array, cabBus){
+  var soln = array.filter(function(driver){
+    return driver.name == cabBus;
+    });
+    return soln;
+    
+}
